f00ad2695f9547fceeff78bc6133005fdd83198f
Ditlev Stjerne <ditlev.stjerne@gmail.com>
2025-10-09 22:15:56 +0200

    command

--- setup-sqlite.sh
+++ setup-sqlite.sh
@@ -4,3 +4,6 @@ uv run sqlite3 ./data/database.db "VACUUM;"
 
 # apply migrations
 uv run alembic upgrade head
+
+# Create a schema migration
+# uv run alembic revision --autogenerate -m "<change description>"
